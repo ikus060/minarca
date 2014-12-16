f88d2634dfd2c089e14e7f6d74a66388413c33ec
Patrik Dufresne <ikus060@gmail.com>
2014-12-16 09:08:54 -0500

    Fix french localization.

--- minarca-ui/src/main/java/com/patrikdufresne/minarca/Localized.java
+++ minarca-ui/src/main/java/com/patrikdufresne/minarca/Localized.java
@@ -7,14 +7,20 @@ package com.patrikdufresne.minarca;
 
 import java.util.Locale;
 
+import org.slf4j.LoggerFactory;
 import org.xnap.commons.i18n.I18n;
 import org.xnap.commons.i18n.I18nFactory;
 
 public class Localized {
 
-    private static final String BUNDLE_NAME = "com.patrikdufresne.minarca.Messages";
+    private static final String BUNDLE_NAME = "com.patrikdufresne.minarca.messages";
 
-    private static I18n i18n = I18nFactory.getI18n(Localized.class, BUNDLE_NAME, Locale.CANADA_FRENCH, I18nFactory.FALLBACK);
+    private static I18n i18n;
+
+    static {
+        LoggerFactory.getLogger(Localized.class).info("using default locale {}", Locale.getDefault());
+        i18n = I18nFactory.getI18n(Localized.class, BUNDLE_NAME, Locale.getDefault(), I18nFactory.DEFAULT);
+    }
 
     public static String _(String text) {
         return i18n.tr(text);
