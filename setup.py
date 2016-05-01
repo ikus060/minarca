4053ae428e5b37e6f4d6fcdeaa98eca46325c201
Patrik Dufresne <ikus060@gmail.com>
2016-05-01 12:28:10 -0400

    Make 'build_less' optional.
    To make rdiffweb work without `lessc` make it optional during build.

--- setup.py
+++ setup.py
@@ -209,7 +209,6 @@ class build(build_):
     sub_commands = build_.sub_commands[:]
     sub_commands.insert(0, ('compile_all_catalogs', None))
     sub_commands.insert(0, ('filltmpl', None))
-    sub_commands.insert(0, ('build_less', None))
 
 setup(
     name="minarca-plugins",
