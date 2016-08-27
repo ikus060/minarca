4e2462861c5311acd23688a7966e69302d4a9543
Patrik Dufresne <ikus060@gmail.com>
2016-08-27 15:39:46 -0400

    Add extra head for branding.

--- minarca_brand/__init__.py
+++ minarca_brand/__init__.py
@@ -11,13 +11,15 @@ from __future__ import unicode_literals
 
 from future.utils import native_str
 import pkg_resources
-from rdiffweb.rdw_plugin import IRdiffwebPlugin
+from rdiffweb.rdw_plugin import ITemplateFilterPlugin
 import cherrypy
 from cherrypy.lib.static import serve_file
 from rdiffweb.dispatch import static
 
+FILES = ['minarca_16.png', 'minarca_22.png', 'minarca_32.png', 'minarca_48.png', 'minarca.ico', 'minarca_128.png']
 
-class MinarcaBrand(IRdiffwebPlugin):
+
+class MinarcaBrand(ITemplateFilterPlugin):
     """
     This plugin update the configuration to use a different header name and
     logo.
@@ -26,10 +28,22 @@ class MinarcaBrand(IRdiffwebPlugin):
         # Override location of main.css
         path = pkg_resources.resource_filename('minarca_brand', 'minarca.css')  # @UndefinedVariable
         self.app.root.static.main_css = static(path)
+
+        # Also add some static files
+        for name in FILES:
+            path = pkg_resources.resource_filename('minarca_brand', name)  # @UndefinedVariable
+            setattr(self.app.root.static, path.replace('.', '_'), static(path))
+
         # In development, provide test.html as static page.
         environment = self.app.cfg.get_config('Environment', 'production')
         if environment != 'production':
             path = pkg_resources.resource_filename('minarca_brand', 'test.html')  # @UndefinedVariable
             self.app.root.static.test_html = static(path)
         # Activate plugin.
-        IRdiffwebPlugin.activate(self)
+        ITemplateFilterPlugin.activate(self)
+
+    def filter_data(self, template_name, data):
+        if template_name.endswith('.html'):
+            # Append our template
+            template = self.app.templates.get_template("minarca_head.html")
+            data["extra_head_templates"].append(template)
