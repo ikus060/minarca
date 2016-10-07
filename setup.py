b6f9dc1df80757951fe5a1c9facecb36bbf96caa
Jenkins <jenkins@icefyre>
2016-10-07 10:28:17 -0400

    Release 0.9.0

--- setup.py
+++ setup.py
@@ -212,7 +212,7 @@ class build(build_):
 
 setup(
     name="minarca-plugins",
-    version='0.9.1',
+    version='0.9.0',
     description='Minarca Plugins',
     long_description='Sets of plugins for Minarca.',
     author='Patrik Dufresne Service Logiciel inc.',
