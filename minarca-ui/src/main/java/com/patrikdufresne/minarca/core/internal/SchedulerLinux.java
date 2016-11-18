61d5675531c760a322016ad0d1001ed5f9fa759b
Patrik Dufresne <ikus060@gmail.com>
2016-11-18 16:46:09 -0500

    Add Linux scheduler

--- minarca-ui/src/main/java/com/patrikdufresne/minarca/core/internal/SchedulerLinux.java
+++ minarca-ui/src/main/java/com/patrikdufresne/minarca/core/internal/SchedulerLinux.java
@@ -5,45 +5,84 @@
  */
 package com.patrikdufresne.minarca.core.internal;
 
+import java.io.IOException;
+import java.util.Calendar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.patrikdufresne.minarca.core.APIException;
-import com.patrikdufresne.minarca.core.Schedule;
-import com.patrikdufresne.minarca.core.APIException.ScheduleNotFoundException;
+import com.patrikdufresne.minarca.core.APIException.MinarcaMissingException;
+import com.patrikdufresne.minarca.core.internal.Crontab.CrontabEntry;
 
 public class SchedulerLinux extends Scheduler {
 
+    private static final transient Logger LOGGER = LoggerFactory.getLogger(SchedulerLinux.class);
+
     @Override
-    public void create(Schedule schedule) {
-        // TODO Auto-generated method stub
+    public void create() throws APIException {
+        LOGGER.debug("creating crontab entry");
+
+        // Create a new date to pick hour and minute.
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.MINUTE, -1);
+        String minute = Integer.toString(c.get(Calendar.MINUTE));
+
+        try {
+            // Delete cronjob if exists.
+            delete();
+            // Create a new entry and add it.
+            Crontab.addEntry(new CrontabEntry(minute, "*", "*", "*", "*", MinarcaExecutable.createMinarcaCommandLine("--backup")));
+        } catch (IOException e) {
+            throw new APIException(e);
+        }
     }
 
     @Override
-    public boolean delete() {
-        // TODO Auto-generated method stub
+    public boolean delete() throws APIException {
+        LOGGER.debug("deleting schedule task");
+        try {
+            // Find a cron tab entry.
+            CrontabEntry entry = find();
+            if (entry != null) {
+                // Delete the entry if exists.
+                Crontab.removeEntry(entry);
+                return true;
+            }
+        } catch (IOException e) {
+            throw new APIException(e);
+        }
         return false;
     }
 
     @Override
-    public boolean exists() throws APIException {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean exists() {
+        try {
+            return find() != null;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
-    @Override
-    public Schedule getSchedule() throws APIException, ScheduleNotFoundException {
-        // TODO Auto-generated method stub
+    /**
+     * Create a matching cron tab entry int the cron tab.
+     * 
+     * @return a cron tab entry or null.
+     * @throws IOException
+     * @throws APIException
+     */
+    protected CrontabEntry find() throws IOException, MinarcaMissingException {
+        // Create the crontab.
+        Crontab crontab = Crontab.readAll();
+        // Identify the entry to be deleted;
+        String command = MinarcaExecutable.createMinarcaCommandLine("--backup");
+        for (CrontabEntry e : crontab.getEntries()) {
+            if (command.equals(e.getCommand())) {
+                return e;
+            }
+        }
+        LOGGER.trace("command line [{}] not found in crontab entries [{}]", command, crontab.getAllEntries().size());
         return null;
-    }
-
-    @Override
-    public void run() throws APIException {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void terminate() throws APIException {
-        // TODO Auto-generated method stub
-
     }
 
 }
