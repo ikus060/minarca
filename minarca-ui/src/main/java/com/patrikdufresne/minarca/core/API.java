8648f86afb12e482fe9f651fe7b70c56e885cf5a
ikus060 <ikus060@ikus060-gb.patrikdufresne.com>
2017-11-03 11:26:20 -0400

    Remove obsolete import.

--- minarca-ui/src/main/java/com/patrikdufresne/minarca/core/API.java
+++ minarca-ui/src/main/java/com/patrikdufresne/minarca/core/API.java
@@ -16,7 +16,6 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
