4d470fe39351a8eee8833d0fe46ccc27523f0751
Patrik Dufresne <info@patrikdufresne.com>
2020-02-13 10:49:44 -0500

    Silently ignore error during inhibit when d-bus is not available #80

--- minarca-core/src/main/java/com/patrikdufresne/minarca/core/API.java
+++ minarca-core/src/main/java/com/patrikdufresne/minarca/core/API.java
@@ -199,6 +199,7 @@ public class API {
         t.setDaemon(true);
         t.start();
 
+        boolean uninhibit = false;
         try {
             checkConfig();
 
@@ -224,6 +225,7 @@ public class API {
             // Prevent Computer from sleeping. during backup.
             try {
                 PowerManagement.inhibit();
+                uninhibit = true;
             } catch (Exception e) {
                 LOGGER.warn("error to inhibit computer");
             }
@@ -249,10 +251,12 @@ public class API {
             throw (e instanceof APIException ? (APIException) e : new APIException(e));
         } finally {
             // Prevent Computer from sleeping.
-            try {
-                PowerManagement.uninhibit();
-            } catch (Exception e) {
-                LOGGER.warn("error to uninhibit computer");
+            if (uninhibit) {
+                try {
+                    PowerManagement.uninhibit();
+                } catch (Exception e) {
+                    LOGGER.warn("error to uninhibit computer");
+                }
             }
         }
 
@@ -323,9 +327,9 @@ public class API {
             case 302:
                 // Try to provide usefule guidance for http vs https URL
                 if (e.getMessage().contains("Location: https://") && baseurl.startsWith("http://")) {
-                    throw new APIException(_(
-                            "{0} is redirected to another location. Double check if the URL should start with https:// instead of http://",
-                            baseurl), e);
+                    throw new APIException(
+                            _("{0} is redirected to another location. Double check if the URL should start with https:// instead of http://", baseurl),
+                            e);
                 }
                 throw new APIException(_("{0} is redirected to another location. Please provide the canonical URL.", baseurl), e);
             case 401:
