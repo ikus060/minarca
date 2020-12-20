6eb79cffab0cc1d7d894e1bc2089d8a386860118
Patrik Dufresne <info@patrikdufresne.com>
2020-12-20 17:12:02 -0500

    Improve integration tests
    * Add minimal windows testing
    * Run include/exclude
    * Run a backup

--- tests/test.sh
+++ tests/test.sh
@@ -13,5 +13,36 @@ MINARCA_USERNAME=${MINARCA_USERNAME:-admin}
 MINARCA_PASSWORD=${MINARCA_PASSWORD:-admin123}
 MINARCA_REPOSITORYNAME=${MINARCA_REPOSITORYNAME:-docker-$HOSTNAME}
 
-# Link minarca
-minarca link --remoteurl $MINARCA_REMOTE_URL --username $MINARCA_USERNAME --password $MINARCA_PASSWORD --name $MINARCA_REPOSITORYNAME
+if [ -z "$WINEPREFIX" ]; then
+
+    # Install minarca client (For Linux)
+    curl --fail -L https://www.ikus-soft.com/archive/minarca/get-minarca.sh -o /tmp/get-minarca.sh
+    bash /tmp/get-minarca.sh --dev --package minarca-client --version "$MINARCA_VERSION"
+    minarca --version
+    
+    # Link minarca
+    minarca link --remoteurl $MINARCA_REMOTE_URL --username $MINARCA_USERNAME --password $MINARCA_PASSWORD --name $MINARCA_REPOSITORYNAME
+    
+    # Include files in backup
+    minarca include /etc /var /home
+    
+    # Backup
+    minarca backup --force
+
+else
+  
+    # Install minarca client (For Windows)
+    MINARCA_INSTALL_FILE=minarca-client_${MINARCA_VERSION}.exe
+    if [ "${MINARCA_VERSION}" = "latest" ]; then
+        MINARCA_INSTALL_FILE=minarca-latest-install.exe;
+    fi
+    curl --fail -L https://www.ikus-soft.com/archive/minarca/${MINARCA_INSTALL_FILE} -o /tmp/minarca-install.exe
+    export DISPLAY=:1
+    (Xvfb :1 -screen 0 1280x960x24 &)
+    (wine /tmp/minarca-install.exe /S || true)
+    echo '#!/bin/bash' > /usr/bin/minarca
+    echo 'wine "C:\\users\\root\\Local Settings\\Application Data\\minarca\\bin\\minarca.exe" "$@"' >> /usr/bin/minarca
+    chmod +x /usr/bin/minarca
+    minarca --version
+
+fi
