4013b074355942b23eb7a5aa03eb82ccef5e7d97
Patrik Dufresne <info@patrikdufresne.com>
2021-11-04 12:51:53 -0400

    Make ScrolledFrame background matches style #163
    Make the cavas background color matches the color of
    TFrame when the theme get updated.

--- minarca-client/minarca_client/ui/tests/snippet_tkvue_scrollable.py
+++ minarca-client/minarca_client/ui/tests/snippet_tkvue_scrollable.py
@@ -1,6 +1,7 @@
 
 
 from minarca_client.ui import tkvue
+from minarca_client.ui.theme import style
 
 
 class RootDialog(tkvue.Component):
@@ -8,8 +9,11 @@ class RootDialog(tkvue.Component):
 <Tk geometry="970x500" title="TKVue Test" className="TKVue">
     <Frame pack-fill="both" pack-expand="true" padding="10">
         <Label text="Static text on top of scrollable" />
-        <ScrolledFrame pack-fill="both" pack-expand="1">
-            <Label pack-fill="x" pack-expand="1" for="i in range(1,100)" text="{{i}}" />
+        <ScrolledFrame pack-fill="both" pack-expand="1" pack-side="left">
+            <Label pack-fill="x" pack-expand="1" for="i in range(1,100)" text="{{ 'left %s' % i }}" />
+        </ScrolledFrame>
+        <ScrolledFrame pack-fill="both" pack-expand="1" pack-side="right">
+            <Label pack-fill="x" pack-expand="1" for="i in range(1,5)" text="{{ 'right %s' % i }}" />
         </ScrolledFrame>
     </Frame>
 </Tk>
@@ -21,6 +25,7 @@ class RootDialog(tkvue.Component):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        style(self.root)
 
 
 if __name__ == "__main__":
