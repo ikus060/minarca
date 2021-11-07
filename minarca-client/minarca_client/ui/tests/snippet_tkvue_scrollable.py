bce11d6d6ab63970f9f6b38837d387ebf4675f05
Patrik Dufresne <info@patrikdufresne.com>
2021-11-07 17:24:49 -0500

    wip

--- minarca-client/minarca_client/ui/tests/snippet_tkvue_scrollable.py
+++ minarca-client/minarca_client/ui/tests/snippet_tkvue_scrollable.py
@@ -5,7 +5,7 @@ from minarca_client.ui import tkvue
 
 class RootDialog(tkvue.Component):
     template = """
-<TopLevel geometry="970x500" title="TKVue Test" className="TKVue">
+<TopLevel geometry="970x500" title="TKVue Test">
     <Frame pack-fill="both" pack-expand="true" padding="10">
         <Label text="Static text on top of scrollable" />
         <ScrolledFrame pack-fill="both" pack-expand="1" pack-side="left">
@@ -24,7 +24,6 @@ class RootDialog(tkvue.Component):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        style(self.root)
 
 
 if __name__ == "__main__":
