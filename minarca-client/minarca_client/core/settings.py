2f06f8d32e0895694f39f5316c306fb39ee16050
Patrik Dufresne <info@patrikdufresne.com>
2024-10-21 13:57:15 -0400

    Split config into settings and status module

---
+++ minarca-client/minarca_client/core/settings.py
@@ -0,0 +1,62 @@
+# Copyright (C) 2024 IKUS Software. All rights reserved.
+# IKUS Software inc. PROPRIETARY/CONFIDENTIAL.
+# Use is subject to license terms.
+'''
+Created on Oct 21, 2024
+
+@author: Patrik Dufresne <patrik@ikus-soft.com>
+'''
+import ast
+import os
+
+from minarca_client.core.config import Datetime, KeyValueConfigFile
+
+
+def list_from_string(value):
+    """
+    Convert string "[5, 6]" to a real python array.
+    """
+    if isinstance(value, list):
+        return value
+    value = ast.literal_eval(value)
+    if isinstance(value, list):
+        return value
+    return []
+
+
+class Settings(KeyValueConfigFile):
+    DAILY = 24
+    HOURLY = 1
+    WEEKLY = 168
+    MONTHLY = 720
+
+    _fields = [
+        ('username', str, None),
+        ('accesstoken', str, None),
+        ('repositoryname', str, None),
+        ('remotehost', str, None),
+        ('remoteurl', str, None),
+        ('remoterole', int, None),
+        ('schedule', int, DAILY),
+        ('configured', lambda x: x in [True, 'true', 'True', '1'], False),
+        ('pause_until', lambda x: Datetime(x) if x else None, None),
+        ('diskid', str, None),
+        ('maxage', int, None),
+        ('ignore_weekday', list_from_string, None),
+        ('keepdays', int, None),
+        ('maxage', int, None),
+        # Load default value from environment variable to ease unittest
+        (
+            'check_latest_version',
+            lambda x: os.environ.get('MINARCA_CHECK_LATEST_VERSION', str(x)).lower() in ['true', 'True', '1'],
+            True,
+        ),
+        ('localmountpoint', str, None),
+        ('localuuid', str, None),
+        ('localrelpath', str, None),
+        ('localcaption', str, None),
+    ]
+
+    @property
+    def remote(self):
+        return self.diskid is None
